f226895bd53885f42e8b61a7ec218c2bc208a269
Daniel Montero <d.montero95@gmail.com>
2018-09-11 21:53:04 -0600

    Bala, y PlayerInfo

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public int damage = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+
+	void onTriggerEnter(Collider other){
+		PlayerInfo player = other.GetComponent<PlayerInfo>();
+		if(player != null){
+			player.Hurt(damage);
+		}
+		Destroy(this.gameObject);
+	}
+}
